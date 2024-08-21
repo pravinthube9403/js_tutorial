1152cb68877e501f8c5b70674d2d213205b6706c
pravinthube9403 <104641086+pravinthube9403@users.noreply.github.com>
2024-08-21 06:25:38 +0000

    dataType

---
+++ 01_Basics_JavaScript/02_dataTypes.js
@@ -0,0 +1,27 @@
+"use strict" //treat all js code as newer version
+//alert(3 + 3) //we  are using nodejs,not browser
+
+console.log(3 + 3); console.log("pravin"); //it works but not for code reduability
+
+console.log(3 + 3);
+console.log("pravin") //this one is ok for code reduability
+
+let name="hitesh" //String variable
+let age=21 //number variable
+let isLoggedIn=false //boolean variable
+let state=null //blank value not undefined
+
+//null==> standalone value
+//undefined ==> 
+//symbol ==> unique
+
+//object 
+
+console.log(typeof "pravin"); //type=String
+console.log(typeof null); //type=object
+console.log(typeof undefined); //type=undefined
+
+
+
+
+
