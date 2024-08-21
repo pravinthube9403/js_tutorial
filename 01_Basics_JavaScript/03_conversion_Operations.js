4353ca048e74dd07d13282c1438ef4dd64390f8a
pravinthube9403 <104641086+pravinthube9403@users.noreply.github.com>
2024-08-21 06:43:40 +0000

    conversionOperation

---
+++ 01_Basics_JavaScript/03_conversion_Operations.js
@@ -0,0 +1,29 @@
+let score="33abc"
+
+//console.log(typeof score);
+
+let valueInNumber=Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber) //result NaN
+//console.log(typeof NaN);
+
+
+// "33" =>33
+//"33abc"=>NaN
+//true=>1; false=0
+
+var isLoggedIn=1
+
+var booleanIsLoggedIn=Boolean(isLoggedIn);
+
+console.log(booleanIsLoggedIn);
+
+//1=>true; 0=>false
+//""=>false
+//"pravin"=>true
+
+let soneNumber=33
+let stringNumber=String(soneNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
